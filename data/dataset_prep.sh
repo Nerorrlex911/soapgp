2ba4315476f838fb85c8c72bedb7b98132b62fe8
Nerorrlex911 <nerorrlex911@outlook.com>
2024-08-01 20:35:44 +0800

    fix babel

--- data/dataset_prep.sh
+++ data/dataset_prep.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 python generate_smiles.py $1  #Generates .can file
 python generate_sdf.py $1 #generates .sdf file using rdkit conformer generation
-babel -isdf $1'.sdf' -oxyz $1'.xyz' #converts to .xyz
+obabel -isdf $1'.sdf' -oxyz $1'.xyz' #converts to .xyz
 python update_xyz.py $1 > $1'_final.xyz' #updates to include smiles names 
 rm $1'.xyz'
 mv $1'_final.xyz' $1'.xyz'
